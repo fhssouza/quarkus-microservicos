907fe3336f04beca4014afad30a981d8357e2733
fhssouza <fhssouza@gmail.com>
2023-10-19 17:28:18 -0300

    product - criando a classe Java entity

---
+++ product/src/main/java/com/souzatech/entity/ProductEntity.java
@@ -0,0 +1,29 @@
+package com.souzatech.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "product")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private String category;
+
+    private BigDecimal price;
+}
